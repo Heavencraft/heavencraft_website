0ac1fa3a2f27577aa438d41a0dfd1f33d36c530e
LRGN <lrgn@protonmail.com>
2017-09-10 14:44:05 +0900

    Remove PHP files

--- templates/footer.inc.php
+++
@@ -1,8 +0,0 @@
-			<div class="row text-center minecraftia">
-				<span class="col-xs-12 menu-item">&copy; Heavencraft 2011-2015</span>
-			</div>
-		</div>
-
-		<div id="authors" class="small text-center">Réalisé par Skilver4 & lorgan17</div>
-	</body>
-</html>
